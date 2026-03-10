68bb25a6ede95b3e17f2a308b3edac99886a679a
Mohamed Ashif <thenameisashif@gmail.com>
2026-03-10 23:49:17 +0530

    Subsequence.py
    Implement a method to check if one string is a subsequence of another.

---
+++ subsequence.java
@@ -0,0 +1,17 @@
+class Solution:
+    def isSubsequence(self, s, t):
+        i = 0
+        j = 0
+
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+            j += 1
+
+        return i == len(s)
+
+
+# Example
+sol = Solution()
+print(sol.isSubsequence("node", "neetcode"))  # True
+print(sol.isSubsequence("axc", "ahbgdc"))     # False
